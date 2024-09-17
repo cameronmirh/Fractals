8e632c7232f128bd2956a929f7b8fc5b74db16ce
Cameron Mirhossaini <53825687+cameronmirh@users.noreply.github.com>
2024-09-17 16:55:12 -0400

    Update fractals.h

--- fractals.h
+++ fractals.h
@@ -1,18 +1,3 @@
-/*
- * CS 106B/X Fractal Recursion Problems
- * This file declares necessary function prototypes, so that other files
- * are able to call the recursive functions you will write.
- * 
- * !!! DO NOT MODIFY THIS FILE !!!
- * !!! DO NOT MODIFY THIS FILE !!!
- * !!! DO NOT MODIFY THIS FILE !!!
- *
- * Please do not modify this provided file. Your turned-in files should work
- * with an unmodified version of all provided code files.
- *
- * (If you want to declare your own helper function prototypes,
- *  do that near the top of your .cpp file, not here.)
- */
 
 #ifndef _recursionproblems_h
 #define _recursionproblems_h
@@ -25,8 +10,6 @@
 #include "complex.h"
 
 // function prototype declarations
-// (If you want to declare your own helper function prototypes, do that near
-//  the top of your .cpp file, not here.  You should NOT modify this file.)
 void drawSierpinskiTriangle(GWindow& window, double x, double y, double size, int order);
 void drawTree(GWindow& window, double x, double y, double size, int order);
 void mandelbrotSet(GWindow& window, double minX, double incX,
@@ -37,9 +20,3 @@ int mandelbrotSetIterations(Complex z, Complex c, int remainingIterations);
 Vector<int> setPalette();
 
 #endif
-
-/*
- * !!! DO NOT MODIFY THIS FILE !!!
- * !!! DO NOT MODIFY THIS FILE !!!
- * !!! DO NOT MODIFY THIS FILE !!!
- */
